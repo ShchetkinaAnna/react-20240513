9396e8c7489836b79ebf2cabb782a605356dcc55
Schetkina, Anna A <shchetkina@parma.ru>
2024-05-22 00:38:50 +0500

    Добавить счетчик в блюдо

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+
+const MAX_DISHES = 5;
+const MIN_DISHES = 0;
+
 export const Dish = ({ dish }) => {
-    return <span>{dish.name}</span>;
-  };
+    const [count, setDishCount] = useState(0);
+
+    const increment = () => {
+      if (count < MAX_DISHES) {
+        setDishCount(count + 1);
+      }
+    };
+    
+    const decrement = () => {
+      if (count > MIN_DISHES) {
+        setDishCount(count - 1);
+      }
+    };
+
+    return (<div>
+      <span>{dish.name}</span>
+      <div>
+        <button onClick={decrement}>-</button>
+        {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </div>);
+};
