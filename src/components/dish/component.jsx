cc22136c4c14b7408228a79fc287d7ac76138674
Schetkina, Anna A <shchetkina@parma.ru>
2024-05-29 01:03:32 +0500

    вынесла хук useCount в отдельный файл и добавила форму для ввода новых отзывов

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,24 +1,10 @@
-import { useCallback, useState } from "react";
+import { useCount } from "../../hooks/use-count";
 
 const MAX_DISHES = 5;
 const MIN_DISHES = 0;
 
-const useCount = () => {
-  const [count, setCount] = useState(MIN_DISHES); 
-
-  const increment = useCallback(() => {
-    setCount((currentCount) => currentCount + 1);
-  }, []);
-  
-  const decrement = useCallback(() => {
-    setCount((currentCount) => currentCount - 1);
-  }, []);
-
-  return { count, increment, decrement };
-}
-
 export const Dish = ({ dish }) => {
-  const { count, increment, decrement } = useCount(); 
+  const { count, increment, decrement } = useCount(MIN_DISHES); 
 
   return (
     <div>
