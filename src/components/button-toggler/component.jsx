08f2efe1aafd81c27b6b2a381725f2877cf273f5
Schetkina, Anna A <shchetkina@parma.ru>
2024-06-03 22:04:45 +0500

    перенесла темы в тогглер

--- src/components/button-toggler/component.jsx
+++ src/components/button-toggler/component.jsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
+
+import { THEMES } from '../../constants/themes';
+import { ThemeContext } from '../../contexts/theme';
 import { StyledButton } from '../styled-button/component';
 
-export const ButtonToggler = ({ onClick, title }) => {
+export const ButtonToggler = ({ onClick }) => {
+  const activeTheme = useContext(ThemeContext);
+
   return (
     <StyledButton style={{ height: '30px' }} onClick={onClick}>
-      {title}
+      {activeTheme === THEMES.light ? 'Светлая тема' : 'Тёмная тема'}
     </StyledButton>
   );
 };
--- src/components/header/component.jsx
+++ src/components/header/component.jsx
@@ -1,21 +1,12 @@
-import { useContext } from 'react';
-
-import { THEMES } from '../../constants/themes';
-import { ThemeContext } from '../../contexts/theme';
 import { AuthorizationButton } from '../authorization-button/component';
 import { ButtonToggler } from '../button-toggler/component';
 
 export const Header = ({ setTheme, setUser }) => {
-  const activeTheme = useContext(ThemeContext);
-
   return (
     <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <h1 style={{ flexGrow: 1 }}>Header</h1>
       <AuthorizationButton setUser={setUser} />
-      <ButtonToggler
-        title={activeTheme === THEMES.light ? 'Светлая тема' : 'Тёмная тема'}
-        onClick={setTheme}
-      />
+      <ButtonToggler onClick={setTheme} />
     </header>
   );
 };
